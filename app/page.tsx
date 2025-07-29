307c151abf62373f1c4460f11701ca5283a22ee9
joannaegay <joannamaeegay@gmail.com>
2025-07-29 13:36:48 +0800

    adding changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
         <Hero />
-        <Skills />
-        <Encryption />
+        {/* <Skills /> */}
+        {/* <Encryption /> */}
         <Projects />
       </div>
     </main>
--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -24,7 +24,7 @@ export const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+            Graphic Designer
           </h1>
         </motion.div>
 
@@ -33,11 +33,11 @@ export const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing{" "}
+            Editable{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              the best
+              Beautiful
             </span>{" "}
-            project experience.
+            Yours in a Click.
           </span>
         </motion.div>
 
@@ -45,8 +45,8 @@ export const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          I&apos;m Mei. Graphic Designer, Templates, Logos & Custom Designs
+Turning ideas into visuals you will love. 
         </motion.p>
 
         <motion.a
@@ -61,14 +61,14 @@ export const HeroContent = () => {
         variants={slideInFromRight(0.8)}
         className="w-full h-full flex justify-center items-center"
       >
-        <Image
+        {/* <Image
           src="/hero-bg.svg"
           alt="work icons"
           height={650}
           width={650}
           draggable={false}
           className="select-none"
-        />
+        /> */}
       </motion.div>
     </motion.div>
   );
